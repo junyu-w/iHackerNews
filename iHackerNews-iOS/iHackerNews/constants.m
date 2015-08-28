b1ee0259fb9cd73df3eed8b43a13a99fdcc423f2
DrakeW <junyuw@berkeley.edu>
2015-08-28 16:28:17 -0700

    .

--- iHackerNews-iOS/iHackerNews/constants.m
+++ iHackerNews-iOS/iHackerNews/constants.m
@@ -10,35 +10,22 @@
 
 @implementation constants
 
-//NSString* const serverURL = @"http://localhost:3000/";
+NSString* const serverURL = @"http://localhost:3000/";
+
+NSString* const getUserURL = @"http://localhost:3000/users/id?";
+NSString* const createUserURL = @"http://localhost:3000/users";
+
+NSString* const markPostURL = @"http://localhost:3000/mark_post";
+NSString* const unmarkPostURL = @"http://localhost:3000/unmark_post";
+
+NSString* const postsOfUserURL = @"http://localhost:3000/posts_of_user";
+NSString* const getDifferentDatesOfPostsURL = @"http://localhost:3000/different_dates_of_posts";
+
 //
-//NSString* const getUserURL = @"http://localhost:3000/users/id?";
-//NSString* const createUserURL = @"http://localhost:3000/users";
+//NSString* const fontForTableViewLight = @"HelveticaNeue-Light";
+//NSString* const fontForTableViewBold = @"HelveticaNeue-Bold";
 //
-//NSString* const markPostURL = @"http://localhost:3000/mark_post";
-//NSString* const unmarkPostURL = @"http://localhost:3000/unmark_post";
-//
-//NSString* const postsOfUserURL = @"http://localhost:3000/posts_of_user";
-//NSString* const getDifferentDatesOfPostsURL = @"http://localhost:3000/different_dates_of_posts";
-
-
-NSString* const serverURL = @"https://fierce-mesa-9720.herokuapp.com/";
-
-NSString* const getUserURL = @"https://fierce-mesa-9720.herokuapp.com/users/id?";
-NSString* const createUserURL = @"https://fierce-mesa-9720.herokuapp.com/users";
-
-NSString* const markPostURL = @"https://fierce-mesa-9720.herokuapp.com/mark_post";
-NSString* const unmarkPostURL = @"https://fierce-mesa-9720.herokuapp.com/unmark_post";
-
-NSString* const postsOfUserURL = @"https://fierce-mesa-9720.herokuapp.com/posts_of_user";
-NSString* const getDifferentDatesOfPostsURL = @"https://fierce-mesa-9720.herokuapp.com/different_dates_of_posts";
-
-
-
-NSString* const fontForTableViewLight = @"HelveticaNeue-Light";
-NSString* const fontForTableViewBold = @"HelveticaNeue-Bold";
-
-NSString* const fontForAppLight = @"HelveticaNeue-Light";
-NSString* const fontForAppBold = @"HelveticaNeue-Bold";
+//NSString* const fontForAppLight = @"HelveticaNeue-Light";
+//NSString* const fontForAppBold = @"HelveticaNeue-Bold";
 
 @end
